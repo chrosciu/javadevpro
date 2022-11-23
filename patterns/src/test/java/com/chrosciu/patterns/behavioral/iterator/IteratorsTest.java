467e933397d9625ae3a16a5c465a432632aab5f2
Marcin Chrost <mchrost82@gmail.com>
2022-11-23 09:09:48 +0100

    Iterator pattern

---
+++ patterns/src/test/java/com/chrosciu/patterns/behavioral/iterator/IteratorsTest.java
@@ -0,0 +1,23 @@
+package com.chrosciu.patterns.behavioral.iterator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorsTest {
+    private final List<Integer> list = List.of(2, 3, 1);
+
+    @Test
+    void regularIteratorTest() {
+        Iterator<Integer> iterator = Iterators.regularIterator(list);
+        Assertions.assertThat(iterator).containsExactly(2, 3, 1);
+    }
+
+    @Test
+    void reverseIteratorTest() {
+        Iterator<Integer> iterator = Iterators.reverseIterator(list);
+        Assertions.assertThat(iterator).containsExactly(1, 3, 2);
+    }
+}
