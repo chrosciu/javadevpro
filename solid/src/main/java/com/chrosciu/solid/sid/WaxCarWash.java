2bf52a931b451bfac48adbd4efa9bc406d05a5a6
Marcin Chrost <mchrost82@gmail.com>
2022-11-28 17:02:08 +0100

    single responsibility, interface segragration, dependency inversion

---
+++ solid/src/main/java/com/chrosciu/solid/sid/WaxCarWash.java
@@ -0,0 +1,26 @@
+package com.chrosciu.solid.sid;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class WaxCarWash {
+    private final Log log;
+    
+    public void wash() {
+        washBody();
+        washWindows();
+        waxBody();
+    }
+
+    private void washBody() {
+        log.addMessage("Washing body");
+    }
+
+    private void washWindows() {
+        log.addMessage("Washing windows");
+    }
+
+    private void waxBody() {
+        log.addMessage("Waxing body");
+    }
+}
