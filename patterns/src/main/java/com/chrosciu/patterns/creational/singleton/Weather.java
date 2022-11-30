80b5bfe1c8b0a97211eb82f7bbfc116d0b10ec98
Marcin Chrost <mchrost82@gmail.com>
2022-11-30 19:28:09 +0100

    singleton pattern - solution

--- patterns/src/main/java/com/chrosciu/patterns/creational/singleton/Weather.java
+++ patterns/src/main/java/com/chrosciu/patterns/creational/singleton/Weather.java
@@ -3,12 +3,19 @@ package com.chrosciu.patterns.creational.singleton;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+import static lombok.AccessLevel.PRIVATE;
+
+@RequiredArgsConstructor(access = PRIVATE)
 @Getter
 public class Weather {
     private final double temperature;
 
+    private static Weather instance = null;
+
     public static Weather getInstance(WeatherService weatherStation) {
-        return new Weather(weatherStation.getTemperature());
+        if (null == instance) {
+            instance = new Weather(weatherStation.getTemperature());
+        }
+        return instance;
     }
 }
