3e56a09600c303e58065012b4e1724031793dbfe
Marcin Chrost <mchrost82@gmail.com>
2023-01-04 23:12:58 +0100

    PaymentService tests with mocks

--- testing/src/test/java/com/chrosciu/shop/products/ProductServiceTest.java
+++ testing/src/test/java/com/chrosciu/shop/products/ProductServiceTest.java
@@ -1,11 +1,18 @@
 package com.chrosciu.shop.products;
 
+import org.assertj.core.api.Assertions;
 import org.javamoney.moneta.FastMoney;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 
-import java.util.Map;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ProductServiceTest {
     private static final Product VIDEO_PRODUCT = Product.builder()
@@ -27,9 +34,9 @@ public class ProductServiceTest {
     @Test
     public void shouldReturnProductWithGivenId() {
         //given
-        Map<Long, Product> productMap = Map.of(VIDEO_PRODUCT_ID, VIDEO_PRODUCT);
-        MapProductRepository productRepository = new MapProductRepository(productMap);
+        ProductRepository productRepository = mock(ProductRepository.class);
         ProductService productService = new ProductService(productRepository);
+        when(productRepository.findById(VIDEO_PRODUCT_ID)).thenReturn(Optional.of(VIDEO_PRODUCT));
 
         //when
         Product product = productService.getBy(VIDEO_PRODUCT_ID);
@@ -39,17 +46,50 @@ public class ProductServiceTest {
     }
 
     @Test
-    public void shouldThrowExceptionIfProductWithGivenIdIsNotFound() {
-        //TODO
+    public void shouldReturnProductWithGivenIdWithLambda() {
+        //given
+        ProductRepository productRepository = mock(ProductRepository.class);
+        ProductService productService = new ProductService(productRepository);
+        doAnswer(invocation -> {
+            long id = invocation.getArgument(0, Long.class);
+            return id == VIDEO_PRODUCT_ID ? Optional.of(VIDEO_PRODUCT) : Optional.empty();
+        }).when(productRepository).findById(anyLong());
+
+        //when
+        Product product = productService.getBy(VIDEO_PRODUCT_ID);
+
+        //then
+        assertThat(product).isEqualTo(VIDEO_PRODUCT);
     }
 
     @Test
     public void shouldSaveProduct() {
-        //TODO
+        //given
+        ProductRepository productRepository = mock(ProductRepository.class);
+        ProductService productService = new ProductService(productRepository);
+
+        //when
+        productService.add(VIDEO_PRODUCT);
+
+        //then
+        Mockito.verify(productRepository).save(VIDEO_PRODUCT);
+        Mockito.verifyNoMoreInteractions(productRepository);
     }
 
     @Test
-    public void shouldReturnAllProducts() {
-        //TODO
+    public void shouldSaveProductWithCaptor() {
+        //given
+        ProductRepository productRepository = mock(ProductRepository.class);
+        ProductService productService = new ProductService(productRepository);
+        ArgumentCaptor<Product> captor = ArgumentCaptor.forClass(Product.class);
+
+        //when
+        productService.add(VIDEO_PRODUCT);
+
+        //then
+        Mockito.verify(productRepository).save(captor.capture());
+        Assertions.assertThat(captor.getValue()).isEqualTo(VIDEO_PRODUCT);
     }
+
+
 }
