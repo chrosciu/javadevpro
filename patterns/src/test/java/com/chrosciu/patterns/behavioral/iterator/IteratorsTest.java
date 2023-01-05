c1d4cbec2df176cbefdb8552f88ef716b3ece91e
Marcin Chrost <mchrost82@gmail.com>
2023-01-05 15:07:12 +0100

    Fix IteratorsTest compilation error

--- patterns/src/test/java/com/chrosciu/patterns/behavioral/iterator/IteratorsTest.java
+++ patterns/src/test/java/com/chrosciu/patterns/behavioral/iterator/IteratorsTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class IteratorsTest {
     private final List<Integer> list = List.of(2, 3, 1);
@@ -12,12 +15,18 @@ public class IteratorsTest {
     @Test
     void regularIteratorTest() {
         Iterator<Integer> iterator = Iterators.regularIterator(list);
-        Assertions.assertThat(iterator).containsExactly(2, 3, 1);
+        Assertions.assertThat(toList(iterator)).containsExactly(2, 3, 1);
     }
 
     @Test
     void reverseIteratorTest() {
         Iterator<Integer> iterator = Iterators.reverseIterator(list);
-        Assertions.assertThat(iterator).containsExactly(1, 3, 2);
+        Assertions.assertThat(toList(iterator)).containsExactly(1, 3, 2);
+    }
+
+    private static <T> List<T> toList(Iterator<? extends T> iterator) {
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(iterator, 0), false)
+                .collect(Collectors.toList());
     }
 }
