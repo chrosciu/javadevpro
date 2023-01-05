27a23dd039bdc30559e50b68d3940b522aa33bcc
Marcin Chrost <mchrost82@gmail.com>
2023-01-05 17:37:42 +0100

    TDD - step 5

--- tdd/src/main/java/com/chrosciu/calculator/StringCalculator.java
+++ tdd/src/main/java/com/chrosciu/calculator/StringCalculator.java
@@ -4,9 +4,6 @@ public class StringCalculator {
     public static int add(String numbers) {
         int returnValue = 0;
         String[] numbersArray = numbers.split(",");
-        if (numbersArray.length > 2) {
-            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
-        }
         for (String number : numbersArray) {
             if (!number.trim().isEmpty()) {
                 returnValue += Integer.parseInt(number);
