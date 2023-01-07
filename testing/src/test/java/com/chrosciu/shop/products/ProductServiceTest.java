69dba9cfd37b5475f05dce518fd05bc86c3761e1
Marcin Chrost <mchrost82@gmail.com>
2023-01-07 14:13:50 +0100

    Added ProductService and its test with in memory repository

---
+++ testing/src/test/java/com/chrosciu/shop/products/ProductServiceTest.java
@@ -0,0 +1,63 @@
+package com.chrosciu.shop.products;
+
+import org.assertj.core.api.Assertions;
+import org.javamoney.moneta.FastMoney;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class ProductServiceTest {
+    private static final Product VIDEO_PRODUCT = Product.builder()
+            .name("Spring masterclass")
+            .description("Praktyczny kurs Spring framework")
+            .type(ProductType.VIDEO)
+            .price(FastMoney.of(1500, "PLN"))
+            .build();
+
+    private static final Product BOOK_PRODUCT = Product.builder()
+            .name("Spring guide")
+            .description("Praktyczne cwiczenia do samodzielnego wykonania")
+            .type(ProductType.BOOK)
+            .price(FastMoney.of(200, "PLN"))
+            .build();
+
+    private static final long VIDEO_PRODUCT_ID = 137L;
+
+    private MapProductRepository productRepository;
+    private ProductService productService;
+
+    @BeforeEach
+    public void setUp() {
+        productRepository = new MapProductRepository();
+        productService = new ProductService(productRepository);
+    }
+
+    @Test
+    public void shouldReturnProductWithGivenId() {
+        //given
+        Map<Long, Product> productMap = Map.of(VIDEO_PRODUCT_ID, VIDEO_PRODUCT);
+        productRepository.setProductMap(productMap);
+
+        //when
+        Product product = productService.getBy(VIDEO_PRODUCT_ID);
+
+        //then
+        Assertions.assertThat(product).isEqualTo(VIDEO_PRODUCT);
+    }
+
+    @Test
+    public void shouldThrowExceptionIfProductWithGivenIdIsNotFound() {
+        //TODO
+    }
+
+    @Test
+    public void shouldSaveProduct() {
+        //TODO
+    }
+
+    @Test
+    public void shouldReturnAllProducts() {
+        //TODO
+    }
+}
