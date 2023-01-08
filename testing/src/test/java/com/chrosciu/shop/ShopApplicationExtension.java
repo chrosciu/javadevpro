414634c13517e0f53cd864cddcbfc757d654ec62
Marcin Chrost <mchrost82@gmail.com>
2023-01-08 14:57:05 +0100

    Junit extension for shop

---
+++ testing/src/test/java/com/chrosciu/shop/ShopApplicationExtension.java
@@ -0,0 +1,32 @@
+package com.chrosciu.shop;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class ShopApplicationExtension implements BeforeEachCallback, AfterEachCallback, ParameterResolver {
+    private ShopApplication shopApplication;
+
+    @Override
+    public void beforeEach(ExtensionContext extensionContext) throws Exception {
+        shopApplication = new ShopApplication();
+    }
+
+    @Override
+    public void afterEach(ExtensionContext extensionContext) throws Exception {
+        shopApplication.close();
+    }
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter().getType() == ShopApplication.class;
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return shopApplication;
+    }
+}
