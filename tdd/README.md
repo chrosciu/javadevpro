92b3743512c36b193f5e50d66782e2f21aa1142a
Marcin Chrost <mchrost82@gmail.com>
2023-01-08 09:47:22 +0100

    TDD - step 4

--- tdd/README.md
+++ tdd/README.md
@@ -1,3 +1,4 @@
 1. Create a simple `StringCalculator` class with a static method `int add(String numbers)`
 2. The `add` method can take 0, 1 or 2 numbers (separated with commas). For example: `` or `1` or `1,2`
-3. For an empty string method should return 0 
+3. For an empty string method should return 0
+4. Method should return sum of passed numbers
--- tdd/src/main/java/com/chrosciu/calculator/StringCalculator.java
+++ tdd/src/main/java/com/chrosciu/calculator/StringCalculator.java
@@ -4,12 +4,17 @@ public class StringCalculator {
     public static int add(String numbers) {
         String[] numbersArray = numbers.split(",");
         validateNumbersCount(numbersArray);
+        return getSum(numbersArray);
+    }
+
+    private static int getSum(String[] numbersArray) {
+        int sum = 0;
         for (String number : numbersArray) {
             if (!number.isEmpty()) {
-                Integer.parseInt(number);
+                sum += Integer.parseInt(number);
             }
         }
-        return 0;
+        return sum;
     }
 
     private static void validateNumbersCount(String[] numbersArray) {
