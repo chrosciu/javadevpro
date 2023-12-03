54184410a67ec8ecce545b03b61fcc30fe815b8c
Marcin Chrost <mchrost82@gmail.com>
2023-12-03 11:40:42 +0100

    State pattern - partial solution

--- patterns/src/main/java/com/chrosciu/patterns/behavioral/state/VendingMachine.java
+++ patterns/src/main/java/com/chrosciu/patterns/behavioral/state/VendingMachine.java
@@ -2,57 +2,89 @@ package com.chrosciu.patterns.behavioral.state;
 
 import lombok.RequiredArgsConstructor;
 
+import static com.chrosciu.patterns.behavioral.state.VendingMachine.State.IDLE;
+import static com.chrosciu.patterns.behavioral.state.VendingMachine.State.IN_PAYMENT;
+import static com.chrosciu.patterns.behavioral.state.VendingMachine.State.AFTER_PAYMENT;
+import static com.chrosciu.patterns.behavioral.state.VendingMachine.State.SOLD_OUT;
+
 @RequiredArgsConstructor
 class VendingMachine {
-    private final int productPrice;
+    enum State {
+        IDLE,
+        IN_PAYMENT,
+        AFTER_PAYMENT,
+        SOLD_OUT
+    }
+
     private int productsAmount = 0;
     private int insertedMoneyAmount = 0;
+    private final int productPrice;
+    private State state = SOLD_OUT;
 
     public String insertMoney(int moneyAmount) {
-        if (productsAmount <= 0) {
-            return "Cannot accept money - items sold out";
-        } else if (insertedMoneyAmount > productPrice) {
-            return "Already paid - please request dispensation";
-        } else {
-            insertedMoneyAmount += moneyAmount;
-            return "Money accepted";
+        switch (state) {
+            case SOLD_OUT:
+                return "Cannot accept money - items sold out";
+            case AFTER_PAYMENT:
+                return "Already paid - please request dispensation";
+            default:
+                insertedMoneyAmount += moneyAmount;
+                if (insertedMoneyAmount < productPrice) {
+                    state = IN_PAYMENT;
+                } else {
+                    state = AFTER_PAYMENT;
+                }
+                return "Money accepted";
+
         }
     }
 
     public String dispenseProduct() {
-        if (productsAmount <= 0) {
-            return "Items sold out";
-        }
-        if (insertedMoneyAmount >= productPrice) {
-            var change = insertedMoneyAmount - productPrice;
-            insertedMoneyAmount = 0;
-            productsAmount--;
-            return String.format("Dispensing product and change %d", change);
-        } else {
-            var toPay = productPrice - insertedMoneyAmount;
-            return String.format("Cannot dispense product - amount %d must be paid", toPay);
+        switch (state) {
+            case SOLD_OUT:
+                return "Items sold out";
+            case AFTER_PAYMENT:
+                var change = insertedMoneyAmount - productPrice;
+                insertedMoneyAmount = 0;
+                productsAmount--;
+                if (productsAmount > 0) {
+                    state = IDLE;
+                } else {
+                    state = SOLD_OUT;
+                }
+                return String.format("Dispensing product and change %d", change);
+            default:
+                var toPay = productPrice - insertedMoneyAmount;
+                return String.format("Cannot dispense product - amount %d must be paid", toPay);
         }
     }
 
     public String cancelPurchase() {
-        if (insertedMoneyAmount > 0) {
-            var toReturn = insertedMoneyAmount;
-            insertedMoneyAmount = 0;
-            return String.format("Returning %d", toReturn);
-        } else {
-            return "Nothing happens";
+        switch (state) {
+            case IN_PAYMENT:
+            case AFTER_PAYMENT:
+                var toReturn = insertedMoneyAmount;
+                insertedMoneyAmount = 0;
+                state = IDLE;
+                return String.format("Returning %d", toReturn);
+            default:
+                return "Nothing happens";
         }
     }
 
     public String refill(int productsBatchAmount) {
-        if (insertedMoneyAmount > 0) {
-            return "Cannot refill - purchase in progress";
-        } else if (productsAmount > 0) {
-            return "Cannot refill - there are still some products";
-        } else {
-            productsAmount = productsBatchAmount;
-            return "Machine refilled";
+        switch (state) {
+            case IDLE:
+                return "Cannot refill - there are still some products";
+            case IN_PAYMENT:
+            case AFTER_PAYMENT:
+                return "Cannot refill - purchase in progress";
+            case SOLD_OUT:
+                productsAmount = productsBatchAmount;
+                state = IDLE;
+                return "Machine refilled";
+            default:
+                throw new IllegalStateException("Unexpected state");
         }
     }
-
 }
