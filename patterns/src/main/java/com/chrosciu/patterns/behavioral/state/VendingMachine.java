1525886ff6757ab90a23bc5abfa74c05e0925db2
Marcin Chrost <mchrost82@gmail.com>
2023-12-04 17:59:38 +0100

    State pattern - final solution

--- patterns/src/main/java/com/chrosciu/patterns/behavioral/state/VendingMachine.java
+++ patterns/src/main/java/com/chrosciu/patterns/behavioral/state/VendingMachine.java
@@ -1,90 +1,144 @@
 package com.chrosciu.patterns.behavioral.state;
 
+import lombok.experimental.Delegate;
 import lombok.RequiredArgsConstructor;
 
-import static com.chrosciu.patterns.behavioral.state.VendingMachine.State.IDLE;
-import static com.chrosciu.patterns.behavioral.state.VendingMachine.State.IN_PAYMENT;
-import static com.chrosciu.patterns.behavioral.state.VendingMachine.State.AFTER_PAYMENT;
-import static com.chrosciu.patterns.behavioral.state.VendingMachine.State.SOLD_OUT;
 
 @RequiredArgsConstructor
 class VendingMachine {
-    enum State {
-        IDLE,
-        IN_PAYMENT,
-        AFTER_PAYMENT,
-        SOLD_OUT
+    interface State {
+        String insertMoney(int moneyAmount);
+        String dispenseProduct();
+        String cancelPurchase();
+        String refill(int productsBatchAmount);
     }
 
     private int productsAmount = 0;
     private int insertedMoneyAmount = 0;
     private final int productPrice;
-    private State state = SOLD_OUT;
+    @Delegate
+    private State state = new SoldOutState();
 
-    public String insertMoney(int moneyAmount) {
-        switch (state) {
-            case SOLD_OUT:
-                return "Cannot accept money - items sold out";
-            case AFTER_PAYMENT:
-                return "Already paid - please request dispensation";
-            default:
-                insertedMoneyAmount += moneyAmount;
-                if (insertedMoneyAmount < productPrice) {
-                    state = IN_PAYMENT;
-                } else {
-                    state = AFTER_PAYMENT;
-                }
-                return "Money accepted";
+    class IdleState implements State {
 
+        @Override
+        public String insertMoney(int moneyAmount) {
+            insertedMoneyAmount += moneyAmount;
+            if (insertedMoneyAmount < productPrice) {
+                state = new InPaymentState();
+            } else {
+                state = new AfterPaymentState();
+            }
+            return "Money accepted";
+        }
+
+        @Override
+        public String dispenseProduct() {
+            var toPay = productPrice - insertedMoneyAmount;
+            return String.format("Cannot dispense product - amount %d must be paid", toPay);
+        }
+
+        @Override
+        public String cancelPurchase() {
+            return "Nothing happens";
+        }
+
+        @Override
+        public String refill(int productsBatchAmount) {
+            return "Cannot refill - there are still some products";
         }
     }
 
-    public String dispenseProduct() {
-        switch (state) {
-            case SOLD_OUT:
-                return "Items sold out";
-            case AFTER_PAYMENT:
-                var change = insertedMoneyAmount - productPrice;
-                insertedMoneyAmount = 0;
-                productsAmount--;
-                if (productsAmount > 0) {
-                    state = IDLE;
-                } else {
-                    state = SOLD_OUT;
-                }
-                return String.format("Dispensing product and change %d", change);
-            default:
-                var toPay = productPrice - insertedMoneyAmount;
-                return String.format("Cannot dispense product - amount %d must be paid", toPay);
+    class InPaymentState implements State {
+
+        @Override
+        public String insertMoney(int moneyAmount) {
+            insertedMoneyAmount += moneyAmount;
+            if (insertedMoneyAmount < productPrice) {
+                state = new InPaymentState();
+            } else {
+                state = new AfterPaymentState();
+            }
+            return "Money accepted";
+        }
+
+        @Override
+        public String dispenseProduct() {
+            var toPay = productPrice - insertedMoneyAmount;
+            return String.format("Cannot dispense product - amount %d must be paid", toPay);
+        }
+
+        @Override
+        public String cancelPurchase() {
+            var toReturn = insertedMoneyAmount;
+            insertedMoneyAmount = 0;
+            state = new IdleState();
+            return String.format("Returning %d", toReturn);
+        }
+
+        @Override
+        public String refill(int productsBatchAmount) {
+            return "Cannot refill - purchase in progress";
         }
     }
 
-    public String cancelPurchase() {
-        switch (state) {
-            case IN_PAYMENT:
-            case AFTER_PAYMENT:
-                var toReturn = insertedMoneyAmount;
-                insertedMoneyAmount = 0;
-                state = IDLE;
-                return String.format("Returning %d", toReturn);
-            default:
-                return "Nothing happens";
+    class AfterPaymentState implements State {
+
+        @Override
+        public String insertMoney(int moneyAmount) {
+            return "Already paid - please request dispensation";
+        }
+
+        @Override
+        public String dispenseProduct() {
+            var change = insertedMoneyAmount - productPrice;
+            insertedMoneyAmount = 0;
+            productsAmount--;
+            if (productsAmount > 0) {
+                state = new IdleState();
+            } else {
+                state = new SoldOutState();
+            }
+            return String.format("Dispensing product and change %d", change);
+        }
+
+        @Override
+        public String cancelPurchase() {
+            var toReturn = insertedMoneyAmount;
+            insertedMoneyAmount = 0;
+            state = new IdleState();
+            return String.format("Returning %d", toReturn);
+        }
+
+        @Override
+        public String refill(int productsBatchAmount) {
+            return "Cannot refill - purchase in progress";
         }
     }
 
-    public String refill(int productsBatchAmount) {
-        switch (state) {
-            case IDLE:
-                return "Cannot refill - there are still some products";
-            case IN_PAYMENT:
-            case AFTER_PAYMENT:
-                return "Cannot refill - purchase in progress";
-            case SOLD_OUT:
-                productsAmount = productsBatchAmount;
-                state = IDLE;
-                return "Machine refilled";
-            default:
-                throw new IllegalStateException("Unexpected state");
+    class SoldOutState implements State {
+
+        @Override
+        public String insertMoney(int moneyAmount) {
+            return "Cannot accept money - items sold out";
+        }
+
+        @Override
+        public String dispenseProduct() {
+            return "Items sold out";
+        }
+
+        @Override
+        public String cancelPurchase() {
+            return "Nothing happens";
+        }
+
+        @Override
+        public String refill(int productsBatchAmount) {
+            productsAmount = productsBatchAmount;
+            state = new IdleState();
+            return "Machine refilled";
         }
     }
+
 }
