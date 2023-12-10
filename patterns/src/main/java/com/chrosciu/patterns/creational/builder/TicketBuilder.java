25b4f220cc79c03f8fc33443a36afda11a411eac
Marcin Chrost <mchrost82@gmail.com>
2023-12-10 21:58:15 +0100

    Builder pattern - solution

--- patterns/src/main/java/com/chrosciu/patterns/creational/builder/TicketBuilder.java
+++ patterns/src/main/java/com/chrosciu/patterns/creational/builder/TicketBuilder.java
@@ -13,19 +13,57 @@ class TicketBuilder {
         FLEX
     }
 
+    private TicketType ticketType;
+    private PriceType priceType;
+
     static TicketBuilder create() {
-        return null;
+        return new TicketBuilder();
     }
 
     TicketBuilder ticketType(TicketType ticketType) {
-        return null;
+        this.ticketType = ticketType;
+        return this;
     }
 
     TicketBuilder priceType(PriceType priceType) {
-        return null;
+        this.priceType = priceType;
+        return this;
     }
 
     Ticket build() {
-        return null;
+        if (null == ticketType) {
+            throw new IllegalStateException("Ticket type is not set");
+        }
+        if (null == priceType) {
+            throw new IllegalStateException("Price type is not set");
+        }
+        Price price = createPrice();
+        return createTicket(price);
+    }
+
+    private Price createPrice() {
+        switch (priceType) {
+            case REGULAR:
+                return new RegularPrice();
+            case DISCOUNT:
+                return new DiscountPrice();
+            case FLEX:
+                return new FlexPrice();
+            default:
+                throw new IllegalStateException("Unknown price type");
+        }
+    }
+
+    private Ticket createTicket(Price price) {
+        switch (ticketType) {
+            case AIR:
+                return new AirTicket(price);
+            case RAILWAY:
+                return new RailwayTicket(price);
+            case FERRY:
+                return new FerryTicket(price);
+            default:
+                throw new IllegalStateException("Unknown ticket type");
+        }
     }
 }
