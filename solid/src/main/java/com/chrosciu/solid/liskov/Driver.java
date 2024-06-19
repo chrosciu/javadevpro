85d4a627291c6858bd0837e0764da9609b94039d
Marcin Chrost <mchrost82@gmail.com>
2024-06-19 13:27:19 +0000

    Liskov substitution principle - incorrect solution

--- solid/src/main/java/com/chrosciu/solid/liskov/Driver.java
+++ solid/src/main/java/com/chrosciu/solid/liskov/Driver.java
@@ -3,6 +3,10 @@ package com.chrosciu.solid.liskov;
 public class Driver {
     public void drive(Vehicle vehicle) {
         vehicle.startEngine();
+        if (vehicle instanceof TeslaVehicle) {
+            TeslaVehicle teslaVehicle = (TeslaVehicle) vehicle;
+            teslaVehicle.chargeBatteries();
+        }
         vehicle.ride();
         vehicle.stopEngine();
     }
